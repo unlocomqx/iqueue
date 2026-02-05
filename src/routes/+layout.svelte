b05e94577d05fb40e1f9863272f3f9c2993d2c06
Tuni-Soft <amxmodx.unloco@gmail.com>
2026-02-05 14:00:45 +0100

    Add PWA icons and update manifest configuration

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,7 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
-  import {onMount} from "svelte"
 
   let { children } = $props();
 </script>
 
-<svelte:head>
-  <link rel="icon" href={favicon} />
-  <link rel="manifest" href="/manifest.json">
-</svelte:head>
 {@render children()}
